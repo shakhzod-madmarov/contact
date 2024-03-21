69345f39882c552e29585302460cbfda6d5187f1
SHAKHZOD <113131580+shakhzod-madmarov@users.noreply.github.com>
2024-03-21 23:21:00 +0500

    added started files and images

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header container">
+      <nav>
+        <a href="/">ALEX SHEVTSOV</a>
+        <ul></ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
